f6aa2be02c5a4fde91872ee71161efd099379a66
Timofeev.Alexey <buzzin@mail.ru>
2018-08-17 11:10:01 +0300

    Change post method

--- post.py
+++ post.py
@@ -3,9 +3,9 @@ import logging
 import postgresql
 import re
 import requests
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 import telegram
 import time
-
 import secret
 
 
@@ -132,7 +132,8 @@ def main():
     count = 1
     count_new = 0
 
-    payload = {'region': '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32, \
+    multipart_data = MultipartEncoder(
+        fields={'region': '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32, \
                33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65, \
                66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91',
                'sex': '',
@@ -141,10 +142,10 @@ def main():
                'family': '',
                'month': '',
                'names': '',
-               'personal_data': '1'}
+               'personal_data': '1'})
 
     s = requests.session()
-    r = s.post("http://www.usynovite.ru/db/", data=payload)
+    r = s.post('http://www.usynovite.ru/db/', data=multipart_data, headers={'Content-Type': multipart_data.content_type})
     html = r.text
     count = parser(html, db, count)
 
