b6bfe2529d2c2a60b6968504bb952d934fb2105b
Timofeev.Alexey <buzzin@mail.ru>
2018-08-22 13:05:44 +0300

    Replace DB to SQLLite

--- post.py
+++ post.py
@@ -1,11 +1,14 @@
 from bs4 import BeautifulSoup
 import logging
-import postgresql
+import os
 import re
 import requests
 from requests_toolbelt.multipart.encoder import MultipartEncoder
+import shutil
+import sqlite3
 import telegram
 import time
+
 import secret
 
 
@@ -14,6 +17,8 @@ def telegram_send_text(msg):
     Send a message to a telegram user specified on chatId
     chat_id must be a number!
     """
+
+    return True
 
     bot = telegram.Bot(secret.token)
     try:
@@ -31,6 +36,8 @@ def telegram_send_image(url):
     chat_id must be a number!
     """
 
+    return True
+
     bot = telegram.Bot(secret.token)
     try:
         bot.send_photo(secret.chat_id, photo=url)
@@ -43,39 +50,81 @@ def telegram_send_image(url):
         return False
 
 
-def select_anketa(db, anketa_id):
+def select_anketa(db_old, anketa_id):
     """
     Проверка наличия в БД номера анкеты
     """
 
-    query = 'SELECT trim(number) FROM anketa WHERE number = \'{0}\''.format(anketa_id)
-    select = db.prepare(query)
-    return 1 if select() else 0
+    c = db_old.cursor()
+    c.execute("select count(*) from anketa where anketa_id = '{0}'".format(anketa_id))
+    count = c.fetchone()
+    if count[0]:
+        return 1
+    else:
+        return 0
 
 
-def insert_anketa(db, anketa_id):
+def insert_anketa(db_new, anketa_id, age):
     """
     Вставка номера анкеты в БД
     """
 
-    query = 'INSERT into anketa (number) values (\'{0}\')'.format(anketa_id)
-    insert = db.prepare(query)
-    insert()
-    logging.info("Анкета {0} добавлена в БД".format(anketa_id))
+    c = db_new.cursor()
+    c.execute("INSERT INTO anketa VALUES ('{0}','{1}')".format(anketa_id, age))
+    db_new.commit()
 
 
 def logging_set():
     """
     Настройка логгирования
     """
-    handlers = [logging.FileHandler('./post.log'), logging.StreamHandler()]
+    handlers = [logging.FileHandler('./logs/post{0}.log'.format(time.strftime("%Y%m%d-%H%M%S"))), logging.StreamHandler()]
     logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',
                         level=logging.INFO,
                         datefmt='%Y%m%d %H%M%S',
                         handlers=handlers)
 
 
-def parser(html, db, count):
+def db_old_connect():
+
+    file_path = './data/usynovite_old.db'
+
+    if os.path.exists(file_path):
+        conn = sqlite3.connect(file_path)
+    else:
+        conn = sqlite3.connect(file_path)
+        c = conn.cursor()
+        c.execute('''CREATE TABLE anketa (anketa_id text, age int)''')
+
+    return conn
+
+
+def db_new_connect():
+    """
+    Подключение к БД
+    """
+
+    file_path = './data/usynovite_new.db'
+
+    if os.path.exists(file_path):
+        conn = sqlite3.connect(file_path)
+    else:
+        conn = sqlite3.connect(file_path)
+        c = conn.cursor()
+        c.execute('''CREATE TABLE anketa (anketa_id text, age int)''')
+
+    return conn
+
+
+def db_close(db_new, db_old):
+
+    db_new.close()
+    db_old.close()
+    shutil.move('./data/usynovite_old.db', './data/usynovite_old_{0}.db'.format(time.strftime("%Y%m%d-%H%M%S")))
+    shutil.move('./data/usynovite_new.db', './data/usynovite_old.db')
+
+
+def parser(html, db_new, db_old, count):
     """
     Парсинг HTML страницы
     """
@@ -93,30 +142,34 @@ def parser(html, db, count):
 
         logging.info("Найдена анкета - {0}: порядковый номер {1}".format(anketa_id, count))
         count += 1
-        if select_anketa(db, anketa_id) == 0:
+
+        msg = re.sub('</p>', '\n', str(k))      # Из <p> делаем перевод строки
+        msg = re.sub('<[^<]+?>', '', msg)       # Убираем все теги HTML
+
+        for i in msg.splitlines():
+            if i.find(' родилась в ') > 0 or i.find(' родился в ') > 0:
+                age = int(i[-4:])
+                break
+
+        msg = 'http://www.usynovite.ru/child/?id={0}\n'.format(anketa_id) + msg
+        image = 'http://www.usynovite.ru/photos/{1}/{0}.jpg'.format(anketa_id, anketa_id[:2])
+
+        logging.info("Год рождения - {0}".format(age))
+
+        if select_anketa(db_old, anketa_id) == 0:
             logging.info("Анкеты {0} нет в БД".format(anketa_id))
-
-            msg = re.sub('</p>', '\n', str(k))      # Из <p> делаем перевод строки
-            msg = re.sub('<[^<]+?>', '', msg)       # Убираем все теги HTML
-
-            for i in msg.splitlines():
-                if i.find(' родилась в ') > 0 or i.find(' родился в ') > 0:
-                    age = int(i[-4:])
-
-            msg = 'http://www.usynovite.ru/child/?id={0}\n'.format(anketa_id) + msg
-            image = 'http://www.usynovite.ru/photos/{1}/{0}.jpg'.format(anketa_id, anketa_id[:2])
-
             if age > 2011:
                 telegram_send_image(image)
                 if telegram_send_text(msg):
-                    insert_anketa(db, anketa_id)
+                    insert_anketa(db_new, anketa_id, age)
+                    logging.info('Анкета {0} добавлена в БД'.format(anketa_id))
                 else:
                     logging.info('Не записываем анкету в БД')
             else:
-                logging.info("Год рождения - {0}".format(age))
-                insert_anketa(db, anketa_id)
-
+                insert_anketa(db_new, anketa_id, age)
+                logging.info('Анкета {0} добавлена в БД'.format(anketa_id))
         else:
+            insert_anketa(db_new, anketa_id, age)
             logging.info("Анкета {0} уже есть в БД".format(anketa_id))
 
     return count
@@ -125,12 +178,12 @@ def parser(html, db, count):
 def main():
 
     logging_set()
-    logging.info("========== Старт ==========")
+    logging.info("========== Start ==========")
 
-    db = postgresql.open(secret.db)
+    db_old = db_old_connect()
+    db_new = db_new_connect()
 
     count = 1
-    count_new = 0
 
     multipart_data = MultipartEncoder(
         fields={'region': '1,2,22,28,29,30,3,31,32,4,33,34,35,36,5,80,75,85,37,19,83,38,6,39,40,41,84,8,9,42,43,10'
@@ -147,21 +200,21 @@ def main():
     s = requests.session()
     r = s.post('http://www.usynovite.ru/db/', data=multipart_data, headers={'Content-Type': multipart_data.content_type})
     html = r.text
-    count = parser(html, db, count)
+    count = parser(html, db_new, db_old, count)
 
     end = 1
     while end > 0:
         r = s.get("http://www.usynovite.ru/db/?p={0}&last-search".format(str(end+1)))
         if (len(r.content) > 55000) and end < 6000:
             html = r.text
-            count = parser(html, db, count)
+            count = parser(html, db_new, db_old, count)
             end += 1
         else:
             end = 0
 
-    db.close()
+    db_close(db_new, db_old)
 
-    logging.info("========== Стоп ==========")
+    logging.info("========== Stop ==========")
 
 
 if __name__ == "__main__":
